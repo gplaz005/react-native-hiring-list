4e674dd1c16f0e92fb8c9ab1eeebd6b1e7cb3a68
gplaz005 <gplaz005@fiu.edu>
2021-02-12 02:44:07 -0500

    display sorted list

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import ListRender from './components/ListRender'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
       <ListRender/>
     </View>
--- components/ListRender.js
+++ components/ListRender.js
@@ -1,27 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View , FlatList, TouchableOpacity} from 'react-native';
+import {useState} from 'react'
 
 
+const ListRender = () => {
+
+const [hiringlist , setHiringList] = useState();
+
+useEffect(() =>{
+getList()
+},[])
+
+// main Function, get Api and sort objects
 const getList = async () => {
     try {
       let response = await fetch(
         'https://fetch-hiring.s3.amazonaws.com/hiring.json'
       );
       let json = await response.json();
-      //console.log(json);
+    
 //return list without nulls or empty strings
      let completeList = []
     json.map((k) =>{
-            //console.log("-------------------------")
             if(k.name !== null){
              if ((k.name).length >= 1){
-            //console.log(k.id)
-            
             completeList = [...completeList, k]
             }
         }})
-        //console.log(completeList)
 
 //return list group by listId
        const groupedList = completeList.sort((a,b) =>{
@@ -32,10 +37,7 @@ const getList = async () => {
            return 0
        })
 
-       //console.log(groupedList)
-
 //return  grouped List sorted by name
-
     const sortedList = groupedList.sort((a,b) =>{
         if(a.listId == b.listId && a.id < b.id){
         return - 1}
@@ -43,22 +45,32 @@ const getList = async () => {
           return 1}
         return 0
     })
-    console.log(sortedList)
-
-      return json;
+    const finalList = JSON.stringify(sortedList)
+   
+    setHiringList(sortedList)
+      return sortedList;
     } catch (error) {
       console.error(error);
     }
   };
 
-getList()
 
 
-
-const ListRender = () => {
   return (
     <View style={styles.container}>
-     <Text>List items</Text> 
+      
+     <Text style={styles.title} >Fetch Rewards</Text> 
+     <FlatList
+     keyExtractor = {(item, index) =>item.id.toString()}
+     data = {hiringlist}
+     renderItem ={itemData => (
+       <View  style ={styles.listItem}>
+         <TouchableOpacity activeOpacity ={0.1}>
+         <Text>ID:{itemData.item.id}       {itemData.item.listId}      {itemData.item.name}</Text>
+         </TouchableOpacity>
+       </View>
+     )}
+     />
     </View>
   );
 }
@@ -68,8 +80,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
+  listItem:{
+    backgroundColor: '#FF8C00',
+    padding: 10,
+    marginBottom: 5,
+    borderWidth: 1,
+    borderRadius:20,
+    width: 300,
+    alignItems: 'center',
+    fontSize: 50
+  },
+  title:{
+    fontSize:35
+  }
 });
 
 export default ListRender;
